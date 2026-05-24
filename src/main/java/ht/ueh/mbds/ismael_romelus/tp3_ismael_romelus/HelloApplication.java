36440dea5a63f9c1f5fc5ef986be5fa9ecf8c3cb
ismamay <ismamayy@yahoo.fr>
2026-05-24 15:19:41 -0400

    premier commit

---
+++ src/main/java/ht/ueh/mbds/ismael_romelus/tp3_ismael_romelus/HelloApplication.java
@@ -0,0 +1,9 @@
+package ht.ueh.mbds.ismael_romelus.tp3_ismael_romelus;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/ht/ueh/mbds/ismael_romelus/tp3_ismael_romelus/HelloResource.java
@@ -0,0 +1,14 @@
+package ht.ueh.mbds.ismael_romelus.tp3_ismael_romelus;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
