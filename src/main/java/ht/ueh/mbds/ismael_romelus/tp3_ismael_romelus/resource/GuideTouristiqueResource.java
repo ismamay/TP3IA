d539a5c33419ace237daab6f95d7d213f2362418
ismamay <ismamayy@yahoo.fr>
2026-05-24 23:20:34 -0400

    creation de resource guidetouristiqueResource... version 1

---
+++ src/main/java/ht/ueh/mbds/ismael_romelus/tp3_ismael_romelus/resource/GuideTouristiqueResource.java
@@ -0,0 +1,24 @@
+package ht.ueh.mbds.ismael_romelus.tp3_ismael_romelus.resource;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/guide")
+public class GuideTouristiqueResource {
+
+    /**
+     * Retourne les informations touristiques d'une ville ou d'un pays au format JSON.
+     *
+     * @param lieu le nom de la ville ou du pays
+     * @return document JSON avec les endroits à visiter et le prix moyen d'un repas
+     */
+    @GET
+    @Path("lieu/{lieu}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String endroitsDeVisite(@PathParam("lieu") String lieu) {
+        return lieu;
+    }
+}
